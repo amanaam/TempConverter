28110ba055fddee2a208184a7a45898750322a1a
Abdul Manaam <amanaam@cfa323-03.andrew.cmu.edu>
2021-09-14 19:09:29 -0400

    start

---
+++ TempConverterApp/TempConverter.swift
@@ -0,0 +1,45 @@
+//
+//  TempConverter.swift
+//  TempConverterApp
+//
+//  Created by Abdul Manaam on 9/14/21.
+//
+
+import Foundation
+
+class TempConverter {
+  var inputTemp: Int = 0
+  var convertedTemp: Int = 0
+  var convertedTempDisplay: String = "0"
+  var tempUnits: String = "°F"
+  var newUnits: String = "°C"
+
+  // Separated functions to check the absolute value cases, by unit
+  private func tempBelowAbsoluteZeroFtoC(temp: Int) -> Bool {
+    return (temp < -454)
+  }
+  
+  private func tempBelowAbsoluteZeroCtoF(temp: Int) -> Bool {
+    return temp < -270
+  }
+  
+  // Separated functions for temperature converstion, by unit
+  private func convertFtoC(temp: Int) -> Int {
+    if tempBelowAbsoluteZeroFtoC(temp: temp) {
+      return -1000
+    }
+    return 5 * (temp - 32) / 9
+  }
+
+  private func convertCtoF(temp: Int) -> Int {
+    if tempBelowAbsoluteZeroCtoF(temp: temp) {
+      return -1000
+    }
+    return (9 * temp) / 5 + 32
+  }
+
+  // Call the appropriate conversion function based on unit and update display variables
+  func convert() {
+  }
+  
+}
