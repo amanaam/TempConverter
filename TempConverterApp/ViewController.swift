a5f92894af22154a2d7153ebeccae29bd517fa65
Abdul Manaam <amanaam@cfa323-10.andrew.cmu.edu>
2021-09-16 16:26:28 -0400

    halfway

--- TempConverterApp/ViewController.swift
+++ TempConverterApp/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  let tempConverter = TempConverter()
+  @IBOutlet weak var inputTempLabel: UILabel!
+  @IBOutlet weak var outputTempLabel: UILabel!
+  @IBOutlet weak var inputTemp: UITextField!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    inputTemp.text = "--"
   }
-
-
+  
+  func updateLabels(<#parameters#>) -> <#return type#> {
+    <#function body#>
+  }
+  
+  func updateDisplay(<#parameters#>) -> <#return type#> {
+    <#function body#>
+  }
+  
+  
+  
+  
+//  @IBAction func startButtonTapped(sender: UIButton) {
+//    Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel), userInfo: nil, repeats: true)
+//
+//    stopwatch.start()
+//  }
+//
+//  @objc func updateElapsedTimeLabel(timer: Timer) {
+//    if stopwatch.isRunning {
+//      elapsedTimeLabel.text = stopwatch.elapsedTimeAsString
+//    } else {
+//      timer.invalidate()
+//    }
+//  }
+//
+//  @IBAction func stopButtonTapped(sender: UIButton) {
+//    stopwatch.stop()
+//  }
 }
 
